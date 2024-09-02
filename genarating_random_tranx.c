5bd2146856375665a6e1454578bc7a0a6dc47abe
BabuRao132 <70302147+BabuRao132@users.noreply.github.com>
2024-09-02 17:27:37 +0530

    non repeated items in tranx

--- genarating_random_tranx.c
+++ genarating_random_tranx.c
@@ -2,24 +2,27 @@
 using namespace std;
 #include<fstream>
 #include <cstdlib>
+#include<set>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	fstream myfile2("tdx.txt",ios::out);
-	int n=6;	// to limit max no.of items per transaction
+	int n=4;	// to limit max no.of items per transaction
 	int x;
-	x=rand()%(10-3+1)+3;
+	x=rand()%(7-3+1)+3;
 	int y;
 	string line;
 	char item;
+	set<char> transactionset;
 	for(int i=1;i<=x;i++){
-		myfile2<<i<<" ";
 		y=rand()%n+1;
-		for (int j=1;j<=y;j++){
+		for (int j=1;j<=y;++j){
 			item=char(rand()%(102-97+1)+97);
-			myfile2<<item<<" ";
+			transactionset.insert(item);
 		}
+		for (auto it = transactionset.begin(); it != transactionset.end(); ++it)
+        	myfile2<<*it<<',';
 		myfile2<<endl;
 	}
 	myfile2.close();
